95bb9acf1969b8ff8706d5e577aa235c093a6893
Shehsank sonji <79108621+sonjishashank@users.noreply.github.com>
2026-02-24 01:42:06 +0530

    Restore picoclaw gateway command in start.sh
    Restore the picoclaw gateway command in start.sh.

--- start.sh
+++ start.sh
@@ -2,12 +2,3 @@
 mkdir -p /home/picoclaw/.picoclaw
 cp /etc/secrets/config.json /home/picoclaw/.picoclaw/config.json
 /usr/local/bin/picoclaw gateway
-```
-
-Commit it.
-
----
-
-### Step 2: Set Docker Command in Render to:
-```
-/bin/sh /start.sh
